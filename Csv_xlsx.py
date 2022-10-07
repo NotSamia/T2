9fa424fcd1b790f404a3c7890c37bc2ccb33a5be
NotSamia <115279993+NotSamia@users.noreply.github.com>
2022-10-07 21:19:30 +0300

    Add files via upload

---
+++ Csv_xlsx.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pandas import ExcelFile
+xlxsfile=pd.read_excel('D:\\my data\\book51 2.xlsx')
+csvfile=pd.read_csv('D:\\my data\\book51 2.csv')
+print(xlxsfile)
+print(csvfile)
